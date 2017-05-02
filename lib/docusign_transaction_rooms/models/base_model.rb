f68c5500c5231d1beb37d568b12bd060e5d00dd5
Mwaki Magotswi <mmagotswi@bostonlogic.com>
2017-05-02 14:55:15 -0400

    eye candy update on model reppresentation on inspect

--- lib/docusign_transaction_rooms/models/base_model.rb
+++ lib/docusign_transaction_rooms/models/base_model.rb
@@ -4,6 +4,12 @@ module DocusignTransactionRooms
 
   class BaseModel
     include Virtus.model
+    include Virtus::Equalizer.new(name || inspect)
+
+    def inspect
+      values = Hash[instance_variables.map { |name| [name, instance_variable_get(name)] } ]
+      "<#{self.class.name} #{values}>"
+    end
   end
 
 end
