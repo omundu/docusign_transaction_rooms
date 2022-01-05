49a482924531ee6bad0798968e61fb2735bd707f
Mwaki Harri Magotswi <magotswi@gmail.com>
2022-01-05 18:49:58 -0500

    updaing supported ruby versions and gem dependancies
    Also making specs a touch more robust

--- test/lib/docusign_transaction_rooms/configuration_test.rb
+++ test/lib/docusign_transaction_rooms/configuration_test.rb
@@ -6,19 +6,26 @@ module DocusignTransactionRooms
     class ModifiedSettings < Minitest::Test
 
       def test_the_api_url_can_be_set
-        assert_equal "https://demo.rooms.docusign.com", DocusignTransactionRooms.configuration.api_url
+        assert_equal 'https://demo.rooms.docusign.com', DocusignTransactionRooms.configuration.api_url
 
-        DocusignTransactionRooms.configure { |config| config.api_url = "https://api.mylittlepony.com"}
+        DocusignTransactionRooms.configure { |config| config.api_url = 'https://api.mylittlepony.com'}
 
-        assert_equal "https://api.mylittlepony.com", DocusignTransactionRooms.configuration.api_url
+        assert_equal 'https://api.mylittlepony.com', DocusignTransactionRooms.configuration.api_url
       end
 
       def test_the_path_url_can_be_set
-        assert_equal "restapi/v1", DocusignTransactionRooms.configuration.path_url
+        assert_equal 'restapi/v1', DocusignTransactionRooms.configuration.path_url
 
-        DocusignTransactionRooms.configure { |config| config.path_url = "v1/towns"}
+        DocusignTransactionRooms.configure { |config| config.path_url = 'v1/towns'}
 
-        assert_equal "v1/towns", DocusignTransactionRooms.configuration.path_url
+        assert_equal 'v1/towns', DocusignTransactionRooms.configuration.path_url
+      end
+
+      def teardown
+        DocusignTransactionRooms.configure do |config|
+          config.api_url  = 'https://demo.rooms.docusign.com'
+          config.path_url = 'restapi/v1'
+        end
       end
 
     end
